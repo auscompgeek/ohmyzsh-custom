b35964fd81b31df09c9b6bb5de625d783bc12181
David <auscompgeek@users.noreply.github.com>
2014-09-27 14:34:54 +1000

    initial commit

---
+++ 10-set-term.zsh
@@ -0,0 +1,75 @@
+# Double-check the damn TERM.
+# Appropriated from http://vim.wikia.com/wiki/256_colors_in_vim.
+
+# Handle pesky terminals that call themselves xterm. (I'm looking at you, libvte.)
+if [ "$TERM" = "xterm" ]; then
+	case "$COLORTERM" in
+		"")
+			case "$XTERM_VERSION" in
+				# My xterm reports itself as XTerm(297). Who knows.
+				"XTerm(256)"|"XTerm(297)") TERM=xterm-256color ;;
+				"XTerm(88)") TERM=xterm-88color ;;
+				"XTerm") ;;
+				"") echo "Warning: Terminal wrongly calling itself 'xterm'." >&2 ;;
+				*) echo "Warning: Unrecognized XTERM_VERSION: $XTERM_VERSION" >&2 ;;
+			esac
+			;;
+
+		# vte3 (GTK+ 3.0)
+		gnome-terminal)
+			TERM=gnome-256color
+			;;
+
+		# vte2 (GTK+ 2.0) - I give up.
+		xfce4-terminal)
+			TERM=xterm-256color
+			;;
+
+		*) echo "Warning: Unrecognized COLORTERM: $COLORTERM" >&2 ;;
+	esac
+fi
+
+# Make sure we've set a TERM that exists in our termcap/terminfo.
+if [ -z "$terminfo[colors]" ]; then
+	case "$TERM" in
+		screen-*color-bce)
+			echo "Unknown terminal $TERM. Falling back to 'screen-bce'." >&2
+			TERM=screen-bce
+			;;
+		screen*) ;;  # Don't touch screen-*color here, let it fall back to 'screen' like it should.
+		*-color)
+			echo "Unknown terminal $TERM. Falling back to 'xterm-color'." >&2
+			TERM=xterm-color
+			;;
+		*-16color)
+			echo "Unknown terminal $TERM. Falling back to 'xterm-16color'." >&2
+			TERM=xterm-16color
+			;;
+		*-88color)
+			echo "Unknown terminal $TERM. Falling back to 'xterm-88color'." >&2
+			TERM=xterm-88color
+			;;
+		*-256color)
+			echo "Unknown terminal $TERM. Falling back to 'xterm-256color'." >&2
+			TERM=xterm-256color
+			;;
+	esac
+fi
+if [ -z "$terminfo[colors]" ]; then
+	case "$TERM" in
+		gnome*|xterm*|konsole*|aterm|[Ee]term|vte*)
+			echo "Unknown terminal $TERM. Falling back to 'xterm'." >&2
+			TERM=xterm
+			;;
+		rxvt*)
+			echo "Unknown terminal $TERM. Falling back to 'rxvt'." >&2
+			TERM=rxvt
+			;;
+		screen*)
+			echo "Unknown terminal $TERM. Falling back to 'screen'." >&2
+			TERM=screen
+			;;
+	esac
+fi
+
+export TERM  # just in case
---
+++ 20-complete-dircolors.zsh
@@ -0,0 +1,19 @@
+setopt completealiases
+
+if [ "$TERM" = fbterm -o "$TERM" = linux ]; then
+	eval "$(TERM=linux dircolors -b)"
+#elif [ "$terminfo[colors]" -lt 256 ]; then
+#	eval "$(dircolors -b)"
+else
+	[ -r ~/.dircolors ] && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+fi
+
+# from debian newuser.zshrc.recommended
+zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*:descriptions' format '%U%F{yellow}%d%f%u'
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+#zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+
+#zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+#zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
