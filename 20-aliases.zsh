2ab5ec269715668169c516a72cfa78bcc9fe9ca3
David <auscompgeek@users.noreply.github.com>
2015-02-21 21:43:58 +1100

    aliases: add mosh ucpu0 alias

--- 20-aliases.zsh
+++ 20-aliases.zsh
@@ -4,5 +4,6 @@ alias l='ls -CF'
 # ssh aliases
 alias sshv="ssh -oProxyCommand='corkscrew 202.139.83.152 8070 %h %p' -oTCPKeepAlive=yes -p443 -C"
 alias ipod='LANG=en_US.UTF-8 LC_CTYPE=en_US.UTF-8 mosh mobile@aucg-ipod.local'
+alias ucpu0="mosh ucpu0 --server='LD_LIBRARY_PATH=\$HOME/.local/lib ~/.local/bin/mosh-server'"
 
 alias -- +x='chmod +x'
